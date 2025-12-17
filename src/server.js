631320197cebd6f517a76930b0f9c1ae2848959f
Roma Galyh <romagalyh@gmail.com>
2025-12-17 23:15:21 +0200

    created base server structure

---
+++ src/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+const PORT = 3000;
+
+export function startServer() {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello World!',
+    });
+  });
+
+  app.use((req, res) => {
+    res.status(404).json({ message: 'Route not found' });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
