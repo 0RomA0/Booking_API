0f0b08f697b3f1c17732e158ba8dba137af52d69
Roma Galyh <romagalyh@gmail.com>
2025-12-22 23:55:46 +0200

    fix errors

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -1,13 +1,21 @@
+import createHttpError from 'http-errors';
 import swaggerUI from 'swagger-ui-express';
-import YAML from 'yamljs';
+import fs from 'node:fs';
 import { SWAGGER_PATH } from '../constants/index.js';
 
 export const swaggerDocs = () => {
   try {
-    const swaggerDocument = YAML.load(SWAGGER_PATH);
+    if (!fs.existsSync(SWAGGER_PATH)) {
+      console.error('Swagger file not found:', SWAGGER_PATH);
+      return (req, res) => res.status(500).send("Can't load swagger docs");
+    }
+
+    const swaggerDocument = JSON.parse(fs.readFileSync(SWAGGER_PATH, 'utf8'));
+
     return swaggerUI.setup(swaggerDocument);
   } catch (err) {
     console.error("Can't load swagger docs:", err);
-    return (req, res) => res.status(500).send("Can't load swagger docs");
+    return (req, res, next) =>
+      next(createHttpError(500, "Can't load swagger docs"));
   }
 };
