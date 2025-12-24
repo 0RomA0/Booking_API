8b8dda2acb483fb45bc8d69700e7a289c79c0102
Roma Galyh <romagalyh@gmail.com>
2025-12-24 13:40:34 +0200

    fix code

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,16 +1,12 @@
 import { logInUser, registerUser } from '../services/auth.js';
 
 export async function registerUserController(req, res) {
-  const user = await registerUser(req.body);
+  const result = await registerUser(req.body);
 
   res.status(200).json({
     status: 200,
     message: 'Successfully registered a user!',
-    data: {
-      id: user._id,
-      email: user.email,
-      role: user.role,
-    },
+    data: result,
   });
 }
 
--- src/services/auth.js
+++ src/services/auth.js
@@ -5,15 +5,33 @@ import jwt from 'jsonwebtoken';
 import { getEnvVar } from '../utils/getEnvVar.js';
 
 export const registerUser = async (payload) => {
-  const user = await UsersCollection.findOne({ email: payload.email });
-  if (user) throw createHttpError(409, 'Email in use');
+  const existingUser = await UsersCollection.findOne({ email: payload.email });
+  if (existingUser) {
+    throw createHttpError(409, 'Email in use');
+  }
 
   const encryptedPassword = await bcrypt.hash(payload.password, 10);
 
-  return await UsersCollection.create({
+  const user = await UsersCollection.create({
     ...payload,
     password: encryptedPassword,
   });
+
+  const token = jwt.sign(
+    { id: user._id, role: user.role },
+    getEnvVar('JWT_SECRET'),
+    { expiresIn: '1d' },
+  );
+
+  return {
+    token,
+    user: {
+      id: user._id,
+      email: user.email,
+      name: user.name,
+      role: user.role,
+    },
+  };
 };
 
 export const logInUser = async ({ email, password }) => {
